9990b560884e9cffc9fc599f8e6a0b7dc5926149
KD <kaitlyn.davis@hashicorp.com>
2024-08-06 12:32:31 -0700

    fixed count ref

--- aws_sg.tf
+++ aws_sg.tf
@@ -5,7 +5,7 @@ resource "aws_security_group" "allow-http" {
 
 # Security Group - Ingress Rule
 resource "aws_vpc_security_group_ingress_rule" "allow-http" {
-  security_group_id = aws_security_group.allow-http[count.index].id
+  security_group_id = aws_security_group.allow-http.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
@@ -14,7 +14,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow-http" {
 
 # Security Group - Egress Rule
 resource "aws_vpc_security_group_egress_rule" "allow-all-traffic" {
-  security_group_id = aws_security_group.allow-http[count.index].id
+  security_group_id = aws_security_group.allow-http.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = -1
 }
